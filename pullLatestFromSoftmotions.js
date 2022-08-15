2e027a03e43c6e3780f51d8abaa5295fbd8e4555
Mark Wylde <me@markwylde.com>
2022-08-15 23:28:29 +0100

    New attempt

--- pullLatestFromSoftmotions.js
+++ pullLatestFromSoftmotions.js
@@ -3,8 +3,6 @@
 const util = require('util');
 const fs = require('fs');
 const exec = util.promisify(require('node:child_process').exec);
-const semver = require('semver');
-const packageJson = require('./package.json');
 
 async function main () {
   const { stdout, stderr } = await exec(
@@ -17,7 +15,7 @@ async function main () {
     throw new Error('git ls-remote failed:', stderr);
   }
 
-  console.log('softmotions/ejdb head:', latestSoftmotionsHead);
+  console.log('softmotions/ejdb head:\n  ', latestSoftmotionsHead + '\n');
   if (latestSoftmotionsHead.length !== 40) {
     throw new Error(`Latest head from SoftMotions was not the expected length`);
   }
@@ -29,6 +27,30 @@ async function main () {
   ].join('\n');
   
   await fs.promises.writeFile('./installSource.sh', newInstallSourceData);
+
+  const currentSoftmotionsHead = installSourceData.split('\n')[0].split('=')[1];
+
+  console.log('markwylde/node-ejdb-lite using:\n  ', currentSoftmotionsHead + '\n');
+
+  if (currentSoftmotionsHead === latestSoftmotionsHead) {
+    console.log('No changes since last update');
+    process.exit(0);
+  }
+
+  {
+    if (!fs.existsSync('ejdb')) {
+      await exec('git clone --recursive https://github.com/Softmotions/ejdb.git');
+    }
+  }
+
+  {
+    const { error, stdout, stderr } = await exec(`
+      git diff --name-only HEAD ${currentSoftmotionsHead}
+    `, { cwd: 'ejdb' });
+
+    console.log('Differences since current release:')
+    console.log(stdout.split('\n').slice(0, -1).map(i => `   - ${i}`).join('\n'));
+  }
 
   await exec('npm vers patch --no-git-tag-version');
 }
