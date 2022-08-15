cce42755f8c73fd67f55e43e1c9f80b0727c2bb9
Mark Wylde <me@markwylde.com>
2022-08-15 05:57:05 +0100

    Implement pullLatestFromSoftmotions.js

--- installSource.sh
+++ installSource.sh
@@ -1,8 +1,9 @@
+LATEST_SOFTMOTIONS_HEAD=b71bf4faa542351dc809c43c5610d6aacd8b6dd7
 rm -rf ejdb
 git clone --depth=1 --recursive https://github.com/Softmotions/ejdb.git
 cd ejdb
-git fetch b71bf4faa542351dc809c43c5610d6aacd8b6dd7
-git checkout b71bf4faa542351dc809c43c5610d6aacd8b6dd7
+git fetch $LATEST_SOFTMOTIONS_HEAD
+git checkout $LATEST_SOFTMOTIONS_HEAD
 mkdir ./build
 cd build
 cmake .. -DBUILD_NODEJS_BINDING=ON -DCMAKE_BUILD_TYPE=Release
